26f4a3dac3375917a287c9c9bbce35b6a1a55f74
Excellence <352481@mail.ru>
2024-02-27 15:57:57 +0300

    Выполнено задание 1

--- dz 15,03/main.py
+++ dz 15,03/main.py
@@ -1,37 +1,75 @@
 """Задание 1
-    Создайте класс для конвертирования температуры из градусов
+    создайте класс для конвертирования температуры из градусов
     Цельсия в градусы Фаренгейта, Кельвина и наоборот. У класса должно быть
     несколько статических методов"""
 from Errors import ValidateFormatFloatError
+
+
 class ConverterTemp:
-    temp_F = 0
-    temp_K = 0
+    """Класс создан для перевода температуры из разных метрических систем
+    в разные
+    Атрибуты класса :
+        - Имя (name) Объекта
+        - Температура (temp) Объекта
+    Класс содержит статические методы :
+        - метод валидации validate_temp
+        - перевод из Цельсия в Фаренгейт - converting_in_F_from_C
+        - перевод из Кельвина в Фаренгейт - converting_in_F_from_K
+        - перевод из Кельвина в Цельсий - converting_in_C_from_K
+        - перевод из Фаренгейта в Цельсий - converting_in_C_from_F
+        - перевод из Цельсия в Кельвин - converting_in_K_from_C
+        - перевод из Фаренгейта в Кельвин - converting_in_K_from_F"""
 
-    def __init__(self, name: str, temp_C: float):
+    def __init__(self, name: str, temp: float):
         self.__name = name
-        self.__temp_C = self.validate_temp_C(temp_C)
+        self.__temp = self.validate_temp(temp)
 
 
     @staticmethod
-    def validate_temp_C(temp_C):
-        if temp_C < -273.5:
+    def validate_temp(__temp):
+        if __temp < -273.5:
             raise ValidateFormatFloatError('Температура не может быть ниже -273.5 градуса Цельсия')
-        return temp_C
-    @property
-    def temp_C(self):
-        return self.__temp_C
+        return __temp
+
     @staticmethod
-    def convertingIn_F(temp_C: float):
+    def converting_in_F_from_C(temp_C: float):
         temp_F = temp_C * 1.8 + 32
-        return float(temp_F)
+        return f'{round(float(temp_F),2)} по Фаренгейту'
 
-    @temp_C.setter
-    def temp_C(self, temp_C):
-        self.__temp_C = self.validate_temp_C(temp_C)
+    @staticmethod
+    def converting_in_F_from_K(temp_K: float):
+        temp_F = temp_K * 9/5 - 459.67
+        return f'{round(float(temp_F),2)} по Фаренгейту'
+
+    @staticmethod
+    def converting_in_C_from_K(temp_K: float):
+        temp_C = temp_K - 273.15
+        return f'{round(float(temp_C), 2)} по Цельсию'
+
+    @staticmethod
+    def converting_in_C_from_F(temp_F: float):
+        temp_C = (temp_F - 32) / 1.8
+        return f'{round(float(temp_C), 2)} по Цельсию'
+
+    @staticmethod
+    def converting_in_K_from_C(temp_C: float):
+        temp_K = temp_C + 273.15
+        return f'{float(temp_K)} по Кельвину'
+
+    @staticmethod
+    def converting_in_K_from_F(temp_F: float):
+        temp_K = (temp_F + 459.67) * 5/9
+        return f'{round(float(temp_K),2)} по Кельвину'
 
 
-temp1 = ConverterTemp('Home', 22.5)
 
-print(temp1.convertingIn_F(100))
-temp1.temp_C = -273.0
-print(temp1.convertingIn_F(100))
+a = ConverterTemp('body', 36.6)
+print(a.converting_in_K_from_C(50))
+print(a.convertingIn_K_from_F(60))
+print(a.convertingIn_F_from_K(300))
+
+
+
+
+"""Задание 2.  Создайте класс для перевода длинны из метрической в 
+имперскую(английскую) систему. У класса должно быть несколько статических методов"""
