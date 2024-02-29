507a33bddfe89d84dd9aaecc515101b7c79357f0
Excellence <352481@mail.ru>
2024-02-29 15:46:40 +0300

    Внесены исправления в задачу point in space

--- dz 12,03 - 1/main.py
+++ dz 12,03 - 1/main.py
@@ -24,7 +24,8 @@ class Auto:
         - свойства атрибутов (@property) для их вывода
         - сеттеры атрибутов (@атрибут.setter) для изменеия значений атрибутов
     """
-
+    equip_auto = {'Коробка передач': "automat", 'Электропакет': "full", 'Тип': "petrol",
+            'Объем двигателя':  1.5, 'Производитель': 'Korea'}
     def __init__(self, mark: str, model: str, year_of_production: int, color: str, price: float,
                  equipment: dict, acceleration: float):
         self.__mark = self.__validate_mark(mark)
@@ -48,32 +49,32 @@ class Auto:
 
     @staticmethod
     def __validate_mark(mark: str) -> str:
-        """Метод для проверки введенной информации в поле mark, проверяется на пустое значение,
-            присутсвие цифр, проверка на тип вводимых данных (строка), а так же на вводимые символы"""
+        """Метод для проверки введенной информации в поле "марка авто", проверяется на пустое значение,
+            присутствие цифр, проверка на тип вводимых данных (строка), а так же на вводимые символы"""
 
+        if not isinstance(mark, str):
+            raise ValidateStrError('Параметр "Марка Авто" должен быть строкой')
         if not mark:
-            raise EmptyNameError('поле "Марка Авто" не может быть пустым')
+            raise EmptyNameError('Параметр "Марка Авто" не может быть пустым')
         if mark.isdigit():
-            raise ValidateFormatStrError('В поле "Марка Авто" не может быть цифр')
+            raise ValidateFormatStrError('Параметр "Марка Авто" не может быть цифр')
         a = list(filter(lambda x: 48 <= ord(x) <= 1103, mark))
         if len(a) != len(mark):
-            raise ValidateFormatStrError('Параметр name должен содержать только символы кириллицы')
-        if not isinstance(mark, str):
-            raise ValidateStrError('Параметр name должен быть строкой')
+            raise ValidateFormatStrError('Параметр "Марка Авто" должен содержать только символы кириллицы')
         return mark.capitalize()
 
     @staticmethod
     def __validate_model(model: str) -> str:
-        """Метод для проверки введенной информации в поле model, проверяется на пустое значение,
+        """Метод для проверки введенной информации в Параметр "Модель авто" проверяется на пустое значение,
             проверка на тип вводимых данных (строка), а так же на вводимые символы"""
 
+        if not isinstance(model, str):
+            raise ValidateStrError('Параметр "Модель авто" должен быть строкой')
         if not model:
-            raise EmptyNameError('поле "Модель Авто" не может быть пустым')
+            raise EmptyNameError('Параметр "Модель авто" не может быть пустым')
         q = list(filter(lambda x: 48 <= ord(x) <= 1103, model))
         if len(q) != len(model):
-            raise ValidateFormatStrError('Параметр model должен содержать только символы кириллицы')
-        if not isinstance(model, str):
-            raise ValidateStrError('Параметр model должен быть строкой')
+            raise ValidateFormatStrError('Параметр "Модель авто" должен содержать только символы кириллицы и английский алфавит')
         return model.capitalize()
 
     @staticmethod
@@ -82,11 +83,11 @@ class Auto:
                      тип вводимых данных (целое число), а так же на верный диапазон года выпуска авто"""
 
         if not year_of_production:
-            raise EmptyNameError('поле "Год выпуска" не может быть пустым')
+            raise EmptyNameError('Параметр "Год выпуска" не может быть пустым')
         if not isinstance(year_of_production, int):
-            raise ValidateIntError('Параметр Год выпуска должен быть целочисленным')
+            raise ValidateIntError('Параметр "Год выпуска" должен быть целочисленным')
         if year_of_production < 2012 or year_of_production > 2024:
-            raise ValidateFormatIntError('Параметр year_of_production должен быть в диапазоне от 2012 до 2024')
+            raise ValidateFormatIntError('Параметр "Год выпуска" должен быть в диапазоне от 2012 до 2024')
         return year_of_production
 
     @staticmethod
@@ -94,35 +95,35 @@ class Auto:
         """Метод для проверки введенной информации в поле color, проверяется на пустое значение,
                     проверка на тип вводимых данных (строка), а так же на вводимые символы"""
 
+        if not isinstance(color, str):
+            raise ValidateStrError('Параметр "Цвет авто" должен быть строкой')
         if not color:
-            raise EmptyNameError('поле "цвет Авто" не может быть пустым')
+            raise EmptyNameError('Параметр "Цвет авто" не может быть пустым')
         q = list(filter(lambda x: 1040 <= ord(x) <= 1103, color))
         if len(q) != len(color):
-            raise ValidateFormatStrError('Параметр color должен содержать только символы кириллицы')
-        if not isinstance(color, str):
-            raise ValidateStrError('Параметр color должен быть строкой')
+            raise ValidateFormatStrError('Параметр "Цвет авто" должен содержать только символы кириллицы')
         return color.capitalize()
 
     @staticmethod
     def __validate_price(price: float) -> float:
-        """Метод для проверки введенной информации в поле price, проверяется на пустое значение,
+        """Метод для проверки введенной информации в параметр "Цена", проверяется на пустое значение,
                             проверка на тип вводимых данных (целочисленное значение)"""
 
-        if not price:
-            raise EmptyNameError('поле price не может быть пустым')
         if not isinstance(price, float):
-            raise ValidateFormatFloatError('Параметр price должен быть числом округленным до сотых')
+            raise ValidateFormatFloatError('Параметр "Цена" должен быть числом округленным до сотых')
+        if not price:
+            raise EmptyNameError('Параметр "Цена" не может быть пустым')
         return price
 
     @staticmethod
     def __validate_acceleration(acceleration: float) -> float:
-        """Метод для проверки введенной информации в поле acceleration, проверяется на пустое значение,
+        """Метод для проверки введенной информации в Параметр "Разгон до 100 км/ч,сек" проверяется на пустое значение,
                                     проверка на тип вводимых данных (число должно быть округлено до десятых)"""
 
-        if not acceleration:
-            raise EmptyNameError('поле "Разгон до 100 км/ч,сек" не может быть пустым')
         if not isinstance(acceleration, float):
             raise ValidateFormatFloatError('Параметр "Разгон до 100 км/ч,сек" должен быть числом округленным до десятых')
+        if not acceleration:
+            raise EmptyNameError('поле "Разгон до 100 км/ч,сек" не может быть пустым')
         return acceleration
 
     @staticmethod
@@ -130,13 +131,13 @@ class Auto:
         """Метод для проверки введенной информации в поле equipment, проверяется на пустое значение,
         так же на вводимое значение ключа в формате str, а значение ключа"""
 
-        if not equipment:
-            raise EmptyNameError('поле equipment не может быть пустым')
+        #if equipment.items() not in equip_:
+         #   raise EmptyNameError('поле equipment не может быть пустым')
         # q = list(filter(lambda x: 48 <= ord(x) <= 1103, equipment.values()))
         # if len(q) == len(equipment.values()):
         #     raise Exception('Параметр values(второй параметр) должен содержать только символы кириллицы, '
         #                     'цифры и английский алфавит')
-        return equipment
+        #return equipment
 
     @property
     def mark(self) -> str:
@@ -219,73 +220,45 @@ class Point_in_space:
     """Класс "Точка в пространстве"
      Класс описывает объект в виде точки в пространстве которая имеет свои координаты
      по трем осям, x, y, z, при помощи методов можно управлять положением точки в пространстве.
-     методы позволяют менять ее координаты, а так же получать иформацию о ее текущем местоположении
+     методы позволяют менять ее координаты, а так же получать информацию о ее текущем местоположении
+     Атрибуты:
+        - designation - "Обозначение"
+        - axis_x, axis_y, axis_z - оси x, y, z
     Класс в себе содержит следующие методы:
     - статик - методы(validate_атрибут) для валидации атрибутов
     - свойства атрибутов( @ property) для их вывода
-    - сеттеры атрибутов( @ атрибут.setter) для изменеия координат точки и изменения типа точки
-      True - истинная, False - Мнимая  """
+    - сеттеры атрибутов( @ атрибут.setter) для изменения координат точки и изменения типа точки
+     """
 
-    def __init__(self, designation: str, axis_x: float, axis_y: float, axis_z: float, point_type: bool):
+    def __init__(self, designation: str, axis_x: float, axis_y: float, axis_z: float):
         self.__designation = self.__validate_designation(designation)
-        self.__axis_x = self.__validate_axis_x(axis_x)
-        self.__axis_y = self.__validate_axis_y(axis_y)
-        self.__axis_z = self.__validate_axis_z(axis_z)
-        self.__point_type = self.__validate_point_type(point_type)
+        self.__axis_x = self.__validate_axis(axis_x)
+        self.__axis_y = self.__validate_axis(axis_y)
+        self.__axis_z = self.__validate_axis(axis_z)
 
     @staticmethod
     def __validate_designation(designation: str):
         """Метод для проверки введенной информации в поле name, проверяется на пустое значение,
                  проверка на тип вводимых данных (строка), а так же проверка на вводимые символы"""
+        if not isinstance(designation, str):
+            raise ValidateStrError('Параметр "обозначение"  должен быть строкой')
         if not designation:
-            raise EmptyNameError('поле designation не может быть пустым')
+            raise EmptyNameError('Параметр "обозначение" не может быть пустым')
         a = list(filter(lambda x: 48 <= ord(x) <= 1103, designation))
         if len(a) != len(designation):
-            raise ValidateFormatStrError('Параметр designation может содержать символы кириллицы, цифры и английский алфавит')
-        if not isinstance(designation, str):
-            raise ValidateStrError('Параметр designation должен быть строкой')
+            raise ValidateFormatStrError('Параметр "обозначение" может содержать символы кириллицы, цифры и английский алфавит')
         return designation.capitalize()
 
     @staticmethod
-    def __validate_axis_x(axis_x: float) -> float:
-        """Метод для проверки введенной информации в поле axis_x, проверяется на пустое значение,
+    def __validate_axis(axis: float) -> float:
+        """Метод для проверки введенной информации в поле ось, проверяется на пустое значение,
                                 проверка на тип вводимых данных (значение с плавающей точкой)"""
+        if not isinstance(axis, float):
+            raise ValidateIntError('Параметр "ось" должен быть числом округленным до десятых')
+        if not axis:
+            raise EmptyNameError('поле "ось" не может быть пустым')
+        return axis
 
-        if not axis_x:
-            raise EmptyNameError('поле "ось х" не может быть пустым')
-        if not isinstance(axis_x, float):
-            raise ValidateIntError('Параметр axis_x должен быть числом')
-        return axis_x
-
-    @staticmethod
-    def __validate_axis_y(axis_y: float) -> float:
-        """Метод для проверки введенной информации в поле axis_y, проверяется на пустое значение,
-                                проверка на тип вводимых данных (значение с плавающей точкой)"""
-
-        if not axis_y:
-            raise EmptyNameError('поле "ось y" не может быть пустым')
-        if not isinstance(axis_y, float):
-            raise ValidateIntError('Параметр axis_y должен быть числом')
-        return axis_y
-
-    @staticmethod
-    def __validate_axis_z(axis_z: float) -> float:
-        """Метод для проверки введенной информации в поле axis_z, проверяется на пустое значение,
-                                проверка на тип вводимых данных (значение с плавающей точкой)"""
-
-        if not axis_z:
-            raise EmptyNameError('поле "ось z" не может быть пустым')
-        if not isinstance(axis_z, float):
-            raise ValidateIntError('Параметр axis_z должен быть числом')
-        return axis_z
-
-    @staticmethod
-    def __validate_point_type(point_type: bool) -> bool:
-        """Метод для проверки введенной информации в поле point_type,
-                проверка на тип вводимых данных (значение True или False)"""
-        if not isinstance(point_type, bool):
-            raise ValidateFormatBoolError('Параметр point_type должен быть True или False')
-        return point_type
 
     def __str__(self):
         """Метод для вывода информации всех значений атрибутов на печать"""
@@ -293,8 +266,7 @@ class Point_in_space:
         return (f'Название точки: {self.__designation} \n'
                 f'Координата оси Х: {self.__axis_x}\n'
                 f'Координата оси Y: {self.__axis_y}\n'
-                f'Координата оси Z: {self.__axis_z}\n'
-                f'Тип точки: {self.__point_type}')
+                f'Координата оси Z: {self.__axis_z}\n')
 
     @property
     def designation(self) -> str:
@@ -310,7 +282,7 @@ class Point_in_space:
 
     @axis_x.setter
     def axis_x(self, axis_x):
-        self.__axis_x = self.__validate_axis_x(axis_x)
+        self.__axis_x = self.__validate_axis(axis_x)
 
     @property
     def axis_y(self) -> float:
@@ -318,7 +290,7 @@ class Point_in_space:
 
     @axis_y.setter
     def axis_y(self, axis_y):
-        self.__axis_y = self.__validate_axis_y(axis_y)
+        self.__axis_y = self.__validate_axis(axis_y)
 
     @property
     def axis_z(self) -> float:
@@ -326,21 +298,11 @@ class Point_in_space:
 
     @axis_z.setter
     def axis_z(self, axis_z):
-        self.__axis_z = self.__validate_axis_z(axis_z)
-
-    @property
-    def point_type(self) -> bool:
-        return self.__point_type
-
-    @point_type.setter
-    def point_type(self, point_type):
-        self.__point_type = self.__validate_point_type(point_type)
+        self.__axis_z = self.__validate_axis(axis_z)
 
 
-star = Point_in_space('sun', 1100.1, 245.9, 1186.7, True)
-print(star.designation)
-print(star)
-star.point_type = False
+star = Point_in_space('sun', 1100.1, 245.9, 1186.7)
+star.axis_x = 3000.8
 print(star.axis_x)
-star.axis_y = 1345.0
-print(star)
+star.axis_y = 5000.9
+print(star.axis_y)
