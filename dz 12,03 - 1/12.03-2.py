4a8fd4cefdd2ea62d2b84b3caded4afec81372fb
Excellence <352481@mail.ru>
2024-03-05 12:27:33 +0300

    дз 12.03-1 выполнено

--- dz 12,03 - 1/12.03-2.py
+++ dz 12,03 - 1/12.03-2.py
@@ -9,7 +9,7 @@ from datetime import date
 from Errors import EmptyNameError
 from Errors import ValidateStrError
 from Errors import ValidateFormatFloatError
-
+from Errors import ValidateFormatStrError
 
 class Student:
     def __init__(self, name: list, date_of_born: date, title_group: str, average_score: float, items: list):
@@ -134,12 +134,12 @@ class Student:
 
 
 
-student1 = Student(['Иванов', 'Иван', 'Иванович'], date(1984, 4, 9), 'П-7', 4.4, ['Математика',"Физика", "Высшая математика"])
-print(student1.name)
-student1.name = ['Петров', 'Петр', 'Петрович']
-print(student1.name)
-student1.append_items('Тригонометрия')
-print(student1.items)
+# student1 = Student(['Иванов', 'Иван', 'Иванович'], date(1984, 4, 9), 'П-7', 4.4, ['Математика',"Физика", "Высшая математика"])
+# print(student1.name)
+# student1.name = ['Петров', 'Петр', 'Петрович']
+# print(student1.name)
+# student1.append_items('Тригонометрия')
+# print(student1.items)
 
 
 
@@ -152,3 +152,165 @@ print(student1.items)
 валидации данных для атрибутов объекта. Реализуйте доступ к отдельным
 атрибутам класса через методы (геттеры и сеттеры), используя декоратор
 @property и @атрибут.setter"""
+
+from datetime import date
+
+class Book:
+    """Класс 'Книга' описывает книги и их параметры. Класс имеет следующие атрибуты:
+        - title_book - Название книги
+        - year_of_release - год выпуска
+        - publisher - Издатель
+        - genre - жанр
+        - autor - Автор
+        - price - цена
+        Так же класс имеет следующие методы:
+        - статик-методы (validate_атрибут) для валидации атрибутов
+        - свойства атрибутов (@property) для их вывода
+        - сеттеры атрибутов (@атрибут.setter) для изменения значений атрибутов"""
+    def __init__(self, title_book: str, year_of_release: date, publisher: str, genre: str, autor: str, price: float):
+        self.__title_book = self.__validate_title_book(title_book)
+        self.__year_of_release = self.__validate_year_of_release(year_of_release)
+        self.__publisher = self.__validate_publisher(publisher)
+        self.__genre = self.__validate_genre(genre)
+        self.__autor = self.__validate_autor(autor)
+        self.__price = self.__validate_price(price)
+
+    def __str__(self):
+        """Метод для вывода информации всех значений атрибутов на печать"""
+        return (f'Название книги: {self.__title_book}\n'
+                f'Год выпуска: {self.__year_of_release}\n'
+                f'Издатель: {self.__publisher}\n'
+                f'Жанр: {self.__genre} \n'
+                f'Автор: {self.__autor} \n'
+                f'Цена книги: {self.__price}')
+
+    @staticmethod
+    def __validate_title_book(title_book: str) -> str:
+        """Метод для проверки введенной информации в Параметр "Название книги" проверяется на пустое значение,
+            проверка на тип вводимых данных (строка), а так же на вводимые символы """
+
+        if not isinstance(title_book, str):
+            raise ValidateStrError('Параметр "Название книги" должен быть строкой')
+        if not title_book:
+            raise EmptyNameError('Параметр "Название книги" не может быть пустым')
+        q = list(filter(lambda x: 1040 <= ord(x) <= 1103, title_book))
+        if len(q) != len(title_book):
+            raise ValidateFormatStrError('Параметр "Издатель" может содержать символы кириллицы, '
+                                         'цифры и английский алфавит')
+        return title_book
+
+    @staticmethod
+    def __validate_year_of_release(year_of_release: date) -> date:
+        """Метод для проверки введенной информации в Параметр "Год выпуска" проверяется на пустое значение,
+            проверка на тип вводимых данных (дата)"""
+
+        if not year_of_release:
+            raise EmptyNameError('Параметр "Год выпуска" не может быть пустым')
+        return year_of_release
+
+    @staticmethod
+    def __validate_publisher(publisher: str) -> str:
+        """Метод для проверки введенной информации в Параметр "Издатель" проверяется на пустое значение,
+            проверка на тип вводимых данных (строка), а так же на вводимые символы"""
+
+        if not isinstance(publisher, str):
+            raise ValidateStrError('Параметр "Издатель" должен быть строкой')
+        if not publisher:
+            raise EmptyNameError('Параметр "Издатель" не может быть пустым')
+        a = list(filter(lambda x: 1040 <= ord(x) <= 1103, publisher))
+        if len(a) != len(publisher):
+            raise ValidateFormatStrError('Параметр "Издатель" может содержать символы кириллицы, '
+                                         'цифры и английский алфавит')
+        return publisher.capitalize()
+
+    @staticmethod
+    def __validate_genre(genre: str) -> str:
+        """Метод для проверки введенной информации в Параметр "Жанр" проверяется на пустое значение,
+                    проверка на тип вводимых данных (строка), а так же на вводимые символы"""
+
+        if not isinstance(genre, str):
+            raise ValidateFormatFloatError('Параметр "Жанр" должен быть строкой')
+        if not genre:
+            raise EmptyNameError('Параметр "Жанр" не может быть пустым')
+        a = list(filter(lambda x: 1040 <= ord(x) <= 1103, genre))
+        if len(a) != len(genre):
+            raise ValidateFormatStrError('Параметр "Жанр" может содержать символы кириллицы, '
+                                         'цифры и английский алфавит')
+        return genre
+
+    @staticmethod
+    def __validate_autor(autor: str) -> str:
+        """Метод для проверки введенной информации в Параметр "Автор" проверяется на пустое значение,
+            проверка на тип вводимых данных (строка), а так же на вводимые символы"""
+        if not isinstance(autor, str):
+            raise ValidateStrError('Параметр "Автор" должен быть списком')
+        if not autor:
+            raise EmptyNameError('Параметр "Автор" не может быть пустым')
+        a = list(filter(lambda x: 1040 <= ord(x) <= 1103, autor))
+        if len(a) != len(autor):
+            raise ValidateFormatStrError('Параметр "Автор" может содержать символы кириллицы, '
+                                         'цифры и английский алфавит')
+        return autor
+
+    @staticmethod
+    def __validate_price(price: float) -> float:
+        """Метод для проверки введенной информации в параметр "Цена", проверяется на пустое значение,
+                            проверка на тип вводимых данных (число округленное до сотых)"""
+
+        if not isinstance(price, float):
+            raise ValidateFormatFloatError('Параметр "Цена" должен быть числом округленным до сотых')
+        if not price:
+            raise EmptyNameError('Параметр "Цена" не может быть пустым')
+        return price
+
+    @property
+    def title_book(self) -> str:
+        return self.__title_book
+
+    @title_book.setter
+    def title_book(self, title_book):
+        self.__title_book = self.__validate_title_book(title_book)
+
+    @property
+    def year_of_release(self) -> date:
+        return self.__year_of_release
+
+    @year_of_release.setter
+    def year_of_release(self, year_of_release):
+        self.__year_of_release = self.__validate_year_of_release(year_of_release)
+
+    @property
+    def publisher(self) -> str:
+        return self.__publisher
+
+    @publisher.setter
+    def publisher(self, publisher):
+        self.__publisher = self.__validate_publisher(publisher)
+
+    @property
+    def genre(self) -> str:
+        return self.__genre
+
+    @genre.setter
+    def genre(self, genre):
+        self.__genre = self.__validate_genre(genre)
+
+    @property
+    def autor(self) -> str:
+        return self.__autor
+
+    @autor.setter
+    def autor(self, autor):
+        self.__autor = self.__validate_autor(autor)
+
+    @property
+    def price(self) -> float:
+        return self.__price
+
+    @price.setter
+    def price(self, price):
+        self.__price = self.__validate_price(price)
+
+
+book1 = Book('Сказки', (1984, 5, 21), 'Сфера', 'Развлекательный', 'Пушкин', 3.62)
+print(book1)
