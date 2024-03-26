dcdc6498662d7ea27fe9f8b2905c76f0ddd1bdfd
Excellence <352481@mail.ru>
2024-03-26 12:56:14 +0300

    Задание от 29.03-1 выполняется

--- 29,03/1.py
+++ 29,03/1.py
@@ -1,25 +1,39 @@
 from datetime import date
 
 class Clock:
+    """Класс имитирует часы.
+    Attributes:
+        - hours: date - часы
+        - minutes: date - минуты
+        - seconds: date - секунды
+    Methods:
+        - _validate_hours - валидация часов
+        - _validate_minutes - валидация минут
+        - _validate_seconds - валидация секунд
+        - tick - метод при вызове сдвигает время на одну секунду вперед. """
+
     def __init__(self, hours: date, minutes: date, seconds: date):
-        self.hours = hours #self.__validate_hours(hours)
-        self.minutes = minutes #self.__validate_minutes(minutes)
-        self.seconds = seconds #self.__validate_seconds(seconds)
+        self.hours = self._validate_hours(hours)
+        self.minutes = self._validate_minutes(minutes)
+        self.seconds = self._validate_seconds(seconds)
 
     @staticmethod
-    def __validate_hours(hours):
+    def _validate_hours(hours):
         if hours > 23 or hours < 0:
             raise ValueError('Параметр часы вне диапазона от 0 до 23')
+        return hours
 
     @staticmethod
-    def __validate_minutes(minutes):
-        if minutes > 23 or minutes < 0:
+    def _validate_minutes(minutes):
+        if minutes > 59 or minutes < 0:
             raise ValueError('Параметр минуты вне диапазона от 0 до 59')
+        return minutes
 
     @staticmethod
-    def __validate_seconds(seconds):
-        if seconds > 23 or seconds < 0:
+    def _validate_seconds(seconds):
+        if seconds > 59 or seconds < 0:
             raise ValueError('Параметр секунды вне диапазона от 0 до 59')
+        return seconds
 
     def tick(self):
         if self.seconds == 59:
@@ -28,44 +42,66 @@ class Clock:
                     self.hours = 0
                     self.minutes = 0
                     self.seconds = 0
-                    widget.advance()
-                    print(widget)
+                    CalendarClockWidget.advance(self)
+                    print(f'{widget}\n')
                 else:
                     self.hours += 1
                     self.minutes = 0
                     self.seconds = 0
-                    print(widget)
+                    print(f'{widget}\n')
             else:
                 self.minutes += 1
                 self.seconds = 0
-                print(widget)
+                print(f'{widget}\n')
         else:
             self.seconds += 1
-            print(widget)
+            print(f'{widget}\n')
+
 class Calendar:
+    """Класс имитирует календарь.
+        Attributes:
+            - year: date - год
+            - month: date - месяц
+            - day: date - день
+        Methods:
+            - _validate_year - валидация годов
+            - _validate_month - валидация месяцев
+            - _validate_day - валидация дней
+            -  advance - метод при вызове сдвигает календарь на один день вперед."""
     def __init__(self, year: date, month: date, day: date):
-        self.year = year #self.__validate_year(year)
-        self.month = month #self.__validate_month(month)
-        self.day = day #self.__validate_day(day)
+        self.year = self._validate_year(year)
+        self.month = self._validate_month(month)
+        self.day = self._validate_day(day)
 
     @staticmethod
-    def __validate_year(year):
+    def _validate_year(year):
         if year > 2100 or year < 0:
             raise ValueError('Параметр год вне диапазона от 0 до 2100')
+        return year
 
     @staticmethod
-    def __validate_month(month):
+    def _validate_month(month):
         if month > 12 or month < 1:
             raise ValueError('Параметр месяц вне диапазона от 1 до 12')
+        return month
 
-    @staticmethod
-    def __validate_day(day):
-        if day > 31 or day < 1:
-            raise ValueError('Параметр вне диапазона от 1 до 31')
+
+    def _validate_day(self, day):
+        if self.month == 2:
+            if day > 28 or day < 1:
+                raise ValueError('Параметр вне диапазона от 1 до 28')
+        a = [1, 3, 5, 7, 8, 10, 12]
+        if self.month in a:
+            if day > 31 or day < 1:
+                raise ValueError('Параметр  день вне диапазона от 1 до 31')
+        else:
+            if day > 30 or day < 1:
+                raise ValueError('Параметр  день вне диапазона от 1 до 30')
+        return day
 
     def advance(self):
         a = [1, 3, 5, 7, 8, 10, 12]
-        if self.month :
+        if self.month in a:
             if self.day == 31:
                 if self.month == 12:
                     self.year += 1
@@ -73,6 +109,7 @@ class Calendar:
                     self.day = 1
                 else:
                     self.month += 1
+                    self.day = 1
             else:
                 self.day = 1
                 self.month += 1
@@ -90,23 +127,22 @@ class Calendar:
             else:
                 self.day += 1
 
-class CalendarClockWidget(Calendar, Clock):
+class CalendarClockWidget(Clock, Calendar):
+
     def __init__(self, year, month, day, hours, minutes, seconds):
-        super().__init__(year, month, day)
         super().__init__(hours, minutes, seconds)
-        self.year = year
-        self.month = month
-        self.day = day
-        self.hours = hours
-        self.minutes = minutes
-        self.seconds = seconds
+        super(Clock, self).__init__(year, month, day)
 
     def __str__(self):
-        return (f'{self.year} {self.month} {self.day}\n'
-                f'{self.hours} : {self.minutes} : {self.seconds}')
+        return (f'{self.year}-{self.month}-{self.day}\n'
+                f'{self.hours}:{self.minutes}:{self.seconds}')
 
-widget = CalendarClockWidget(2024, 5, 31, 23, 59, 57)
-widget.tick()
-widget.tick()
-widget.tick()
-widget.tick()
+widget = CalendarClockWidget(2024, 12, 31, 23, 59, 58)
+
+def test(gadget):
+    gadget.tick()
+    gadget.tick()
+    gadget.tick()
+    gadget.tick()
+
+test(widget)
