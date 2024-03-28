ec37baca492f7562e3d0fe54f81c8dbfd9290071
Excellence <352481@mail.ru>
2024-03-28 10:45:17 +0300

    Исправлено 15,03-1 первое задание

--- dz 15,03/15.03.-1.py
+++ dz 15,03/15.03.-1.py
@@ -6,27 +6,14 @@ from Errors import ValidateFormatFloatError
 
 
 class ConverterTemp:
-    #FIXME: перечитайте еще раз описание и поправьте
-    """Класс создан для перевода температуры из разных метрических систем
-    в разные
-    Атрибуты класса :
-        - Имя (name) Объекта
-        - Температура (temp) Объекта
-    Класс содержит статические методы :
+    """Класс описывает конвертирование температуры из градусов
+    Цельсия в градусы Фаренгейта, Кельвина и наоборот.
+    Methods :
         - метод валидации validate_temp
         - перевод из Цельсия в Фаренгейт - converting_in_F_from_C
-        - перевод из Кельвина в Фаренгейт - converting_in_F_from_K
-        - перевод из Кельвина в Цельсий - converting_in_C_from_K
         - перевод из Фаренгейта в Цельсий - converting_in_C_from_F
         - перевод из Цельсия в Кельвин - converting_in_K_from_C
-        - перевод из Фаренгейта в Кельвин - converting_in_K_from_F
-        """
-    #FIXME: в этом классе конструктор не нужен, хоть один метод в классе задействует эти атрибуты?
-    def __init__(self, name: str, temp: float):
-        self.__name = name
-        self.__temp = self.validate_temp(temp)
-
-
+        - перевод из Кельвина в Цельсий - converting_in_C_from_K """
 
     @staticmethod
     def validate_temp(__temp):
@@ -34,48 +21,29 @@ class ConverterTemp:
             raise ValidateFormatFloatError('Температура не может быть ниже -273.5 градуса Цельсия')
         return __temp
 
-    #FIXME: Метод должен вернуть значение, а не вывод, это обычный перевод одного значения в другое
     @staticmethod
     def converting_in_F_from_C(temp_C: float):
         temp_F = temp_C * 1.8 + 32
-        return f'{round(float(temp_F),2)} по Фаренгейту'
+        return temp_F
 
-    # FIXME: Метод должен вернуть значение, а не вывод, это обычный перевод одного значения в другое
-    @staticmethod
-    def converting_in_F_from_K(temp_K: float):
-        temp_F = temp_K * 9/5 - 459.67
-        return f'{round(float(temp_F),2)} по Фаренгейту'
-
-    # FIXME: Метод должен вернуть значение, а не вывод, это обычный перевод одного значения в другое
     @staticmethod
     def converting_in_C_from_K(temp_K: float):
         temp_C = temp_K - 273.15
-        return f'{round(float(temp_C), 2)} по Цельсию'
+        return temp_C
 
-    # FIXME: Метод должен вернуть значение, а не вывод, это обычный перевод одного значения в другое
     @staticmethod
     def converting_in_C_from_F(temp_F: float):
         temp_C = (temp_F - 32) / 1.8
-        return f'{round(float(temp_C), 2)} по Цельсию'
+        return temp_C
 
-    # FIXME: Метод должен вернуть значение, а не вывод, это обычный перевод одного значения в другое
     @staticmethod
     def converting_in_K_from_C(temp_C: float):
         temp_K = temp_C + 273.15
-        return f'{float(temp_K)} по Кельвину'
-
-    # FIXME: Метод должен вернуть значение, а не вывод, это обычный перевод одного значения в другое
-    @staticmethod
-    def converting_in_K_from_F(temp_F: float):
-        temp_K = (temp_F + 459.67) * 5/9
-        return f'{round(float(temp_K),2)} по Кельвину'
+        return temp_K
 
 
-a = ConverterTemp('body', 36.6)
-print(a.converting_in_K_from_C(50))
-print(a.converting_in_F_from_K(60))
-print(a.converting_in_F_from_K(300))
-print(a.converting_in_C_from_F(333))
+print(ConverterTemp.converting_in_K_from_C(50))
+print(ConverterTemp.converting_in_C_from_F(333))
 
 
 
