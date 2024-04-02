98571e4e6c6567179c9a86530a9ef416badc7995
Excellence <352481@mail.ru>
2024-04-02 16:49:49 +0300

    задание 2.04 выполняется

--- 2,04/2.04-1.py
+++ 2,04/2.04-1.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 """Задание 1.
 Рассмотрим принцип единственной ответственности на следующем
 примере. Допустим у нас есть класс RentCarService и в нем есть несколько
@@ -6,38 +7,118 @@
 машине, отправить сообщение клиенту с информацией о его брони."""
 
 class Car:
-    ...
+    def __init__(self, car_number: str):
+        self._car_number = car_number
+
+    def __str__(self):
+        return f'Номер авто {self._car_number}'
+
+    @property
+    def car_number(self):
+        return self._car_number
+
+car1 = Car('A999RUS')
+car2 = Car('A888RUS')
+car3 = Car('A777RUS')
+car4 = Car('A666RUS')
+car5 = Car('A555RUS')
+
+cars = [car1, car2, car3, car4, car5]
+cars2 = []
 
 class Client:
-    ...
+    def __init__(self, name: str, telephone: str):
+        self._name = name
+        self._telephone = telephone
+
+    def __str__(self):
+        return (f' {self._name}\n'
+                f'Телефон: {self._telephone}')
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def phone(self):
+        return self._telephone
+
+    @property
+    def telephone(self):
+        return self._telephone
+
+
+client1 = Client('Иван', '123456')
+client2 = Client('Василий', '454575')
+
 
 class Order:
-    ...
+    @staticmethod
+    def print_order(car: Car):
+        print(f'Заказ оформлен!\n печать заказа: ')
+
 
 
 class RentCarService:
-    def __init__(self):
-        self.__cars: list[Car] = []
 
-    def search_car(self, car_number: str):
-        ...
+    @staticmethod
+    def search_car(car: list):
+        if len(car) == 0:
+            return False
+        else:
+            print('Найден свободный автомобиль!')
+            last = car.pop()
+            return last
 
-    def reservation_car(self, car_number: str, client: Client):
-        ...
+    @staticmethod
+    def reservation_car(cars: list, client: Client):
+        if RentCarService.search_car:
+            order = (client, cars)
+            print('Авто зарезервировано')
+            return order
+        else:
+            return print('Нет свободных авто!')
 
 
 class CarPrintService:
-    def print_order(self, order: Order):
-        ...
+    @staticmethod
+    def print_order(order: Order):
+        print(order)
+
 
 class CarInfoService:
     def car_info(self, car: Car):
-        ...
+        return self.car_info(car)
 
-class NotificationService:
+
+class NotificationService(ABC):
     def send_message(self, message: str, client: Client):
         ...
 
+
+class EmailNotification(NotificationService):
+    @staticmethod
+    def send_mail(message: str, client: Client):
+        print(f'Клиенту {client} отправлено сообщение по E-MAIL'
+              f': {message}')
+
+
+class MobileNotification(NotificationService):
+    @staticmethod
+    def send_sms(message: str, client: Client):
+        print(f'Клиенту {client} отправлено сообщение по SMS'
+              f': {message}')
+
+
+def zakaz(client: Client, all_cars: list):
+    find_car = RentCarService.search_car(all_cars)
+    result = RentCarService.reservation_car(find_car, client)
+    Order.print_order(result)
+    EmailNotification.send_mail('Заявка отправлена', client)
+
+zakaz(client1, cars)
+
+#zakaz(client2, cars2)
 
 
 """Необходимо создать класс CarPrintService и вынести туда функционал
