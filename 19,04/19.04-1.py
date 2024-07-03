85c9760c633083b68b2e06d1d7a2822665681257
Excellence <352481@mail.ru>
2024-07-03 16:48:06 +0300

    19.04 начато выполнение

--- 19,04/19.04-1.py
+++ 19,04/19.04-1.py
@@ -1,7 +1,7 @@
-"""Задание 1
+"""Задание 1.
 Создайте класс «Число», которое хранит его значение и информацию о
 системе счисления. Создайте несколько экземпляров данного класса.
-Задание 2
+Задание 2.
 Создайте класс «Калькулятор СС». В классе должна быть реализована
 следующая функциональность:
 – Перевод числа в восьмеричную систему счисления.
@@ -13,3 +13,41 @@
 записать в СС левого операнда."""
 
 
+class Number:
+
+    @staticmethod
+    def convert_8(x):
+        result = ''
+        while x > 0:
+            result += str(x % 8)
+            x = x // 8
+        result = int(result[::-1])
+        return result
+
+    @staticmethod
+    def convert_16(x):
+        result = ''
+        while x > 0:
+            result += str(x % 16)
+            x = x // 16
+        result = int(result[::-1])
+        return result
+
+    @staticmethod
+    def convert_2(x):
+        result = ''
+        while x > 0:
+            result += str(x % 2)
+            x = x // 2
+        result = int(result[::-1])
+        return result
+
+# Универсальный конвертер, вторая переменная - система исчисления
+#     @staticmethod
+#     def universal_convert(x, y):
+#         result = ''
+#         while x > 0:
+#             result += str(x % y)
+#             x = x // y
+#         result = result[::-1]
+#         return result
