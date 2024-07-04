828598bfcf2b4541a1fe50c09892b50d30ef4394
Excellence <352481@mail.ru>
2024-07-04 10:51:48 +0300

    Задание от 23.04 - 1 выполнено

--- 23,04/23.04-1.py
+++ 23,04/23.04-1.py
@@ -1,4 +1,4 @@
-"""Задание 1
+"""Задание 1.
 Создайте класс очереди для работы с объектами определенного типа.
 Очередь должна иметь определенный размер.
 Реализуйте набор операций для работы с очередью:
@@ -7,13 +7,13 @@
  Удаление элемента из очереди;
  Очистить очередь;
  Получение значения из вершины из очереди.
-Задание 2
+Задание 2.
 Разработать приложение, имитирующее очередь печати принтера. На
 принтер отправляются документы, которые печатаются в порядке очереди.
 Память принтера ограничена. Необходимо сохранять статистику печати
 документа (время начала печати, название документа) в отдельный файл.
 Продемонстрируйте работу с печатью нескольких документов."""
-
+from datetime import datetime
 class Queue:
 
     def __init__(self, len_max=3):
@@ -50,47 +50,57 @@ class Queue:
 queue = Queue()
 class Printer(Queue):
 
-    file = 'doc.txt'
-
     def __init__(self, queue: Queue):
         super().__init__()
         self._queue = queue
+
+    @staticmethod
+    def get_time():
+        new_date = datetime.now()
+        start_time = new_date.time()
+        return start_time
 
     def __str__(self):
         return f'Очередь: {self._data}'
 
     @staticmethod
-    def save_doc(path: str):
-        ...
+    def save_doc(file: str, start_time: str):
+        with open('statistic.txt', 'a', encoding='utf-8') as stat:
+            stat.write(f'Документ: {file},')
+            stat.write(f' Время начала печати: {start_time}\n')
 
-docs = ["doc1.txt", "doc2.txt", "doc3.txt", "doc4.txt"]
-docs1 = ['doc9']
+
+docs = ["doc1.txt", "doc2.txt", "doc3.txt", "doc4.txt", "doc5.txt", "doc6.txt", "doc7.txt"]
 xerox = Printer(queue=3)
 
 def print_doc(docs: list):
     for i in docs:
         if xerox.is_full():
-            print(xerox.is_full())
+            print('Очередь наполнена, идет печать')
             x = xerox.dequeue()
             print(f'{x} напечатан')
-            xerox.save_doc(x)
+            xerox.enqueue(i)
+            print(f'{i} отправлен на печать')
+            xerox.save_doc(i, Printer.get_time())
         else:
             xerox.enqueue(i)
             print(f'{i} отправлен на печать')
+            xerox.save_doc(i, Printer.get_time())
     while not xerox.is_empty():
         x = xerox.dequeue()
         print(f'{x} напечатан')
 
 
-#print_doc(docs)
+def main():
+    try:
+        print_doc(docs)
+    except OSError as e:
+            print(e)
+    except TypeError as e:
+        print(e)
+    else:
+        print('Работа функции завершена успешно!')
+if __name__ == '__main__':
+     main()
 
-print(xerox)
-print(xerox.peek())
-xerox.enqueue("doc1.txt")
-print(xerox.peek())
-xerox.enqueue("doc2.txt")
-xerox.enqueue('doc3.txt')
-xerox.enqueue("doc4.txt")
-print(xerox.peek())
-print(xerox.is_empty())
-print(xerox)
+
